fd1a2518f0731f1095131a59c97c36c7daf644a0
Pichizinho <arthurprzygocki170403@gmail.com>
2026-08-13 10:39:43 -0300

    Correcao

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,10 @@
-
-
-function Input(props){
-    return(
-    <input className="border border-slate-300 outline-slate-400 py-2 rounded-md"
-        {...props}
-        />
-        )
+function Input(props) {
+  return (
+    <input
+      className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      {...props}
+    />
+  );
 }
 
 export default Input;
